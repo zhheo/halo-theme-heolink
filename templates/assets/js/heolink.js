83ebdea77a1a6dfcc2031a119537db33cc9c792a
张洪Heo <zhheo@qq.com>
2024-06-20 11:15:37 +0800

    0.8b3

--- templates/assets/js/heolink.js
+++ templates/assets/js/heolink.js
@@ -2,16 +2,16 @@ var heolink = {
   initNavLink: function() {
     // 获取所有的导航链接
     const navLinks = document.querySelectorAll(".left_nav_link_group_title");
-
+  
     // 为每个链接添加点击事件监听器
     navLinks.forEach(link => {
       link.addEventListener("click", function (event) {
         event.preventDefault(); // 阻止默认的链接跳转行为
-
+  
         // 获取目标元素的ID
         const targetId = this.getAttribute("data-target");
         const targetElement = document.querySelector(`#${targetId}`);
-
+  
         if (targetElement) {
           // 如果目标元素存在，使用 scrollIntoView 方法进行平滑滚动
           targetElement.scrollIntoView({
@@ -21,15 +21,25 @@ var heolink = {
         } else {
           // 如果目标元素不存在，存储目标ID到本地存储
           localStorage.setItem('targetId', targetId);
-          // 跳转到首页
-          window.location.href = "/";
+          
+          // 检查当前URL是否包含 ?preview-theme=theme-heolink 参数
+          const currentUrlParams = new URLSearchParams(window.location.search);
+          let redirectUrl = "/"; // 默认跳转到首页
+          
+          if (currentUrlParams.has("preview-theme") && currentUrlParams.get("preview-theme") === "theme-heolink") {
+            // 如果包含指定的参数，则在跳转时附加参数
+            redirectUrl += "?preview-theme=theme-heolink";
+          }
+          
+          // 跳转到首页或包含参数的首页
+          window.location.href = redirectUrl;
         }
       });
     });
-
+  
     // 页面加载时检查本地存储中的任务
     this.checkAndExecuteStoredTask();
-  },
+  },  
 
   checkAndExecuteStoredTask: function() {
     // 检查是否有存储的目标ID
